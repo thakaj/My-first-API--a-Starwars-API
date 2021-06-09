8ccf3fe6beadd2dc13119e49964f72fd54f0f30f
thakaj <taulant.hakaj@scranton.edu>
2021-06-08 20:53:57 -0400

    planning phase

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('hello')
+// The user will have a button that shows additional information about each character from the API
+//event listener 1: when the user clicks a button on the page additional information is showed
+//When the character info is shown there is a hide button
+//event listener 2: when the user clicks a button the information is hidden 
+//the user will be able to click a like button on each characters profile
+//event listener 3: when the user clicks a button there is an active response on the page
+//stretch goal: add a comments section for users to leave anonomyus comments
