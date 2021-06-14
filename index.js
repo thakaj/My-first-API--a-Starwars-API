08a554da12f1c3e09bf6c88e04a8907d88c50b15
thakaj <taulant.hakaj@scranton.edu>
2021-06-14 18:13:43 -0400

    lots and lots of changes to the js specifically around the event listeners. Added and delted some html

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
-// look at html.index and plan what the page looks like when it loads
-//switch between html and js as you work
-
-//things to do!!!!!!!! in order!
-//add a display info btn and a hide info btn into the header of each character name and also have a hide character info button
-//set event listeners for the buttons make sure the functionality is there
-//next would be to properly pull the data and apply it to the website
-//then i should make it dynamic so when the event listener is trigered the data populates or hides
-
-
-
-//write a function that will handle the data i get back from res.results write a function that creates a div and appends
-//display 7 key:values from results with a for each statement
-//use hiden function for the hide display button with if statements to hide old message
-//i need to iterate through the objects and have it displayed on the html rather than writing html out right
 
 function characterInfo(arrayObjects){
+    let counter = 0;
     arrayObjects.forEach(character => {
-        //create a div element and save it as an element
-        //set that div elements innerhtml  => interpolate the data
-        //appened it to the container div
         const newDiv = document.createElement("article")
         newDiv.setAttribute("class", "content-block")
         newDiv.innerHTML = `
-            <h2>${character.name} <button id= "btn">Use the force!</button> </h2>
-            <div id= "hide-div">
+            <h2>${character.name}</h2>
+            <div id = "${counter}">
             <article> Height: ${character.height}
             <div>
             Mass: ${character.mass}
@@ -52,24 +35,29 @@ function characterInfo(arrayObjects){
             </div>
             `
         document.getElementById("character").appendChild(newDiv)
-        //could search doc. ill get multiple btn
-        //querselector very cool. can call on div
-        //newDiv.queryselector => event listner
     });
 }
 
+const hideBtn = document.getElementById("hide-btn")
+hideBtn.addEventListener('click',function infoDisplay(click){
+    let divs = document.getElementsByClassName("content-block");
+    for (var i = 0; i < divs.length; i++) {
+        divs.item(i).style.display = "none";
+    }
+})
+
+const showBtn = document.getElementById("show-btn")
+showBtn.addEventListener('click',function infoDisplay(click){
+    let divs = document.getElementsByClassName("content-block");
+    for (var i = 0; i < divs.length; i++) {
+        divs.item(i).style.display = "";
+    }
+})
 
 
 
 
-//create a button and append. can append all day
-//instead innertext user innerHTml because u can write everything inside of a string
-//
 
-
-
-// The user will have a button that shows additional information about each character from the API
-//define the api
 
 function fetchApi(){
     fetch("https://swapi.dev/api/people/")
